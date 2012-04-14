154db5e09cfd4145736bd402aaa4a63483633194
15 Storeys <justin_lust@yahoo.com>
2012-04-14 20:36:41 +0100

    Signed-off-by: 15 Storeys <justin_lust@yahoo.com>

--- js/walk.js
+++ js/walk.js
@@ -1,163 +1,14 @@
-var map, geocoder, marker, ey, my, mouseDown = false;
+var map, fullroute, geocoder, marker, ey, my, mouseDown = false;
 var o = {
 	init: function(){
 		// in this place we call all needed functions
 		this.map.init();
 		this.northdowns.addDayMarkers();
-		this.northdowns.addRoutes();
+		var route = this.northdowns.addRoutes();
+		$.when(route).then(
+		    function(){ o.picasa.getAlbum('NorthDownsWay'); }
+		);
 	}, // end o.init
-	
-/*	twitter: {
-		getTwitterUser: function() {
-			var users = o.twitter.getEnteredUser();
-			o.twitter.rate_limit_status_callback(function(remaining) {
-				if (remaining > 10) {
-					o.twitter.show(users);
-				}
-				else {
-					alert ('Twitter request limited exceeded');
-				}
-			});
-			
-		},
-		
-		getTwitterUserFriends: function() {
-			var user = $('.enteruser').find('input').val();
-			if (user != ""){
-				o.twitter.rate_limit_status_callback(function(remaining) {
-					if (remaining > 10) {
-						var friendsurl = 'http://api.twitter.com/1/friends/ids.json?&screen_name='+encodeURIComponent(user)+'&callback=?';
-						$.getJSON(friendsurl, function(data) {
-							//use data.ids to call users function to then retrieve more data
-							var usersurl = 'http://api.twitter.com/1/users/lookup.json?user_id='+data.ids+'&include_entities=true&callback=?';
-							$.getJSON(usersurl, function(data) {
-								// create markers and do other stuff with these objects
-								console.log ('user follows '+data.length+' people');
-								// TODO add paging for retriving blocks of 100 accounts
-								if (data.length < 100) {
-									for (i in data) {
-										var user = data[i];
-										o.twitter.addMarker(user);
-									}
-								}
-								
-							});
-						});
-					}
-					else {
-						alert ('Twitter request limited exceeded');
-					}
-				});
-			}
-		},
-		
-
-		addMarker: function(user)
-		{
-			var img = user.profile_image_url,
-			    screen_name = user.screen_name;
-			geocoder.geocode({ address: user.location }, function(response, status){
-				if (status == google.maps.GeocoderStatus.OK) {
-					var x = response[0].geometry.location.lat(),
-					    y = response[0].geometry.location.lng();
-					console.log('geocode ok ' + screen_name + ' '  + user.location);    
-					// Create marker on map, with twitter photo
-					marker = new google.maps.Marker({
-						icon: img,
-						map: map,
-						title: screen_name,
-						position: new google.maps.LatLng(x,y)
-					}); // end of marker
-					
-					google.maps.event.addListener(marker, 'click', function(){
-						// when marker is clicked, call our twitter.open
-						o.twitter.open(this.title);
-					});// end of marker click listener
-				}	
-				else
-				{
-					console.log('geocode bad ' + screen_name + ' '  + user.location);    
-				}
-		    	}); // end of geocode function
-		},
-		
-		getEnteredUser: function() {
-			var arr = new Array();
-			arr[0] = $('.enteruser').find('input').val();
-			return arr;
-		},
-		
-		rate_limit_status_callback: function(callback){
-			// called by functions before they run
-			$.getJSON('http://api.twitter.com/1/account/rate_limit_status.json?callback=?', function(data) {
-				remaining = data.remaining_hits;
-				callback(remaining);
-			});
-		},
-		
-		rate_limit_status: function() {
-			// called directly from link on webpage
-			var remamining;
-			$.getJSON('http://api.twitter.com/1/account/rate_limit_status.json?callback=?', function(data) {
-				remaining = data.remaining_hits;
-				alert('remaining = ' + remaining);
-			});
-		},
-		
-		show: function(users) {
-			for (i in users){
-				var user = users[i];
-				$.getJSON('http://twitter.com/users/show/'+user+'.json?callback=?', function(data) {
-					var img = data.profile_image_url,
-					    screen_name = data.screen_name;
-					// TODO refactor this to use the addMarker function to avoid repeating code
-					geocoder.geocode({ address: data.location }, function(response, status){
-						if (status == google.maps.GeocoderStatus.OK) {
-							var x = response[0].geometry.location.lat(),
-							    y = response[0].geometry.location.lng();
-
-							// Create marker on map, with twitter photo
-							marker = new google.maps.Marker({
-								icon: img,
-								map: map,
-								title: screen_name,
-								position: new google.maps.LatLng(x,y)
-							}); // end of marker
-						
-							google.maps.event.addListener(marker, 'click', function(){
-								// when marker is clicked, call our twitter.open
-								o.twitter.open(this.title);
-							});// end of marker click listener
-						} // end of geocode success block
-					}); // end of geocode function
-				}); // end of getJSON function		
-			} // iterate users
-			
-		}, // end of twitter.show function
-		
-		click: function() {
-			$('.twitter').find('.open').live('click', function() {
-				var t = $(this), rel= t.attr('rel');
-				o.twitter.open(rel);
-			});
-		}, // end of twitter.click function
-		
-		open: function(user) {
-			var posts =$('.posts'), arr = new Array;
-			$.getJSON('http://twitter.com/status/user_timeline/'+user+'.json?count=5&callback=?', function(data) {
-				$.each(data, function(i, post) {
-					arr.push('<div class="post">');
-					arr.push(post.text);
-					arr.push('</div>');					
-				}); // end each post loop
-				var html = arr.join('');
-				posts.html(html).fadeIn();
-			});// end of getJSON function
-		} // end of twitter.open function
-		
-	}, // end of o.twitter
-
-*/
 	
 	map: {
 		size: function(){
@@ -185,6 +36,111 @@ var o = {
 		} 
 	}, //end o.map
 
+	picasa: {
+		 getAlbum: function(albumName) {
+			var albumUrl = 'https://picasaweb.google.com/data/feed/api/user/kris.coverdale@gmail.com/album/NorthDownsWay?imgmax=912&callback=?';
+			$.getJSON(albumUrl, function(data) {
+				
+				var photos = [];
+				$(data).find("entry").each(function()
+				{
+					var photo = [];
+					
+					// get image url
+					photo[0] = $(this).children('content').attr("src");
+					
+					//get timedate taken
+					photo[1] = $(this).children('gphoto\\:timestamp').html();
+				
+					// get caption
+					photo[2] = $(this).children('summary').html();
+					
+					photos.push(photo);
+					//get thumbnail urls
+					//entry.media:group.thumbnail(s)
+				});
+		
+				// hold onto first photo and loop through polyline looking for match.
+				// once found output photo on map
+				// then increment to next photo and continue along line
+				var routePoint = 0;
+				for(i = 0; i < photos.length; i++)
+				{
+					var photo = photos[i];
+					var photoPlaced = 'false';
+					photoTime = photo[1];
+
+					photoTime = photoTime - 2000000
+					
+					// look for photo time value in full route array
+					var latlong = [];
+					latlong = o.picasa.binarySearch(photoTime);
+					console.log('lat: ' + latlong[0]);
+					console.log('lon: ' + latlong[1]);
+					
+					/* while(photoPlaced == 'false' && routePoint < fullroute.length) {
+						
+						if (i>5)
+						{
+							console.log ('debug');
+						}
+						var gpx = fullroute[routePoint];
+						var nextgpx = fullroute[routePoint+1];
+						
+						pointTime = o.picasa.unixtime(gpx[2]);
+						nextPointTime = o.picasa.unixtime(nextgpx[2]);
+						
+						if (photoTime > pointTime && photoTime <= nextPointTime) {
+							photoPlaced='true';
+							console.log('photo: ' + i);
+							console.log('lat: ' + gpx[0]);
+							console.log('lon: ' + gpx[1]);
+							console.log('url: ' + photo[0]);
+							console.log('caption: ' + photo[2]);
+							
+							//if photofound, don't advance on the line, as there may be more here
+							routePoint--;
+						}
+						routePoint++;
+					} */
+				}
+				
+			});
+			
+		}, // end o.picasa.getAlbum 
+		
+		unixtime: function (timestamp) {
+			var myDate = new Date(timestamp);
+			return myDate.getTime();
+		},
+		
+		binarySearch: function (value) {
+			var latlong = [];
+			var startIndex = 0;
+			var stopIndex = fullroute.length - 1;
+			var middle = Math.floor((stopIndex + startIndex)/2);
+			
+			while(!(o.picasa.unixtime(fullroute[middle][2]) < value && o.picasa.unixtime(fullroute[middle+1][2]) >= value) && startIndex < stopIndex){
+			
+				//adjust search area
+				if (value < o.picasa.unixtime(fullroute[middle][2])){
+				    stopIndex = middle - 1;
+				} else if (value > o.picasa.unixtime(fullroute[middle][2])){
+				    startIndex = middle + 1;
+				}
+
+				//recalculate middle
+				middle = Math.floor((stopIndex + startIndex)/2);
+			}
+			
+			    //make sure it's the right value
+			//return (items[middle] != value) ? -1 : middle;
+			latlong [0] = fullroute[middle][0];
+			latlong [1] = fullroute[middle][1];
+			return latlong;
+		}
+	},//end o.picasa 
+	
 	northdowns: {
 		addDayMarkers: function (){
 		
@@ -230,7 +186,7 @@ var o = {
 					"data/RK_gpx _2012-03-24_0955.gpx",
 					"data/RK_gpx _2012-03-31_0921.gpx",
 					"data/RK_gpx _2012-04-08_0905.gpx"];
-		
+			fullroute = [];
 			for (i = 0; i < gpxRoutes.length; i++) {
 				this.addRoute(gpxRoutes[i]);
 			}
@@ -261,7 +217,12 @@ var o = {
 			// Loop through co-ords in GPX and plot onto map
 			$(xml).find("trkpt").each(function()
 			{
+				// for polyline
 				coordinates.push(new google.maps.LatLng($(this).attr("lat"), $(this).attr("lon")));
+			
+				// store in global variable to use for photo placement later
+				var gpx = [$(this).attr("lat"), $(this).attr("lon"), $(this).children('time').text()];
+				fullroute.push(gpx);
 			});
 		
 			  var route = new google.maps.Polyline({
